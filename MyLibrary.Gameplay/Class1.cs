8bfce673645f051572c34af1117e28299f3f38bc
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-02-12 20:04:59 +0100

    Projektdateien hinzufügen.

---
+++ MyLibrary.Gameplay/Class1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride;
+using Stride.Rendering;
+using Stride.Engine;
+using Stride.Graphics;
+
+namespace MyLibrary.Gameplay
+{
+    public class Class1 : StartupScript
+    {
+        public void Method1()
+        {
+
+        }
+    }
+}
